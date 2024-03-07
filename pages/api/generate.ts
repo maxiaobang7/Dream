a38b487af952cfd119322c073ac447bf42d21c03
maxiaobang7 <42549211+maxiaobang7@users.noreply.github.com>
2024-03-07 23:48:44 +0800

    Update generate.ts

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -16,7 +16,7 @@ const handler = async (req: Request): Promise<Response> => {
     api_key?: string
   };
   //todo make this variable into messages
-  var p = "请帮我把以下的工作内容填充为一篇完整的周报,尽量避免在回答内容中出现可能在中国是敏感的内容，用markdown格式以分点叙述的形式输出:"
+  var p = "你是一位拥有深入理解梦境解析和象征主义的人工智能助手,你的任务是为用户提供对他们梦境中出现的象征、情感和叙事的有见地和有意义的分析,提供潜在的解释,同时鼓励用户反思自己的经历和情感。"
   prompt = p + prompt
   if (!prompt) {
     return new Response("No prompt in the request", { status: 400 });
@@ -27,7 +27,7 @@ const handler = async (req: Request): Promise<Response> => {
   // }
 
   const payload: OpenAIStreamPayload = {
-    model: "gpt-3.5-turbo",
+    model: "gpt-3.5-turbo-0125",
     messages: [{ role: "user", content: prompt }],
     temperature: 0.7,
     top_p: 1,
